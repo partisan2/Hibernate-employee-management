ce8e63d045679d5a361706e131b5dcaa6e5a23cc
Vihan Gayathra <s92068813@ousl.lk>
2023-12-14 23:53:12 +0530

    add

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import model.Student;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student student = new Student("vihan@com", "vihan", "amarasingha");
+		System.out.println(student.toString());
+
+	}
+
+}
